4866faba1a8fec4c53505876fcd34b8cddf25b15
Parvez Fakruddin <parvez@jet.com>
2016-07-21 15:17:49 -0400

    Added snappy compression, compression code crashes

--- tests/kafunk.Tests/Compression-gzip-tests.fs
+++ tests/kafunk.Tests/Compression-gzip-tests.fs
@@ -15,10 +15,32 @@ let ``Message Compression with Gzip`` () =
     let message2 = Message.create (Binary.ofArray message2Bytes) None None
     
     let inputMessage =
-        Compression.gzip [message; message2]
+        Compression.compressMessages [message; message2] Protocol.CompressionCodec.GZIP
 
     let outputMessageSet =
-        Compression.decompressMessage inputMessage
+        Compression.decompressMessage inputMessage Protocol.CompressionCodec.GZIP
+
+    let messages = outputMessageSet.messages
+    Assert.IsTrue (messages.Length = 2)
+    let (offset, size, msg) = messages.[0]
+    let (offset2, size2, msg2) = messages.[1]
+    Assert.IsTrue (msg.value |> Binary.toArray = messageBytes)
+    Assert.IsTrue (msg2.value |> Binary.toArray = message2Bytes)
+
+[<Test>]
+[<Category("Compression")>]
+let ``Message Compression with Snappy`` () =
+    let messageBytes = [| 1uy; 2uy; 3uy; 4uy; 2uy; 6uy; 8uy |]
+    let message2Bytes = [| 1uy; 2uy; 3uy; 2uy |]
+
+    let message = Message.create (Binary.ofArray messageBytes) None None
+    let message2 = Message.create (Binary.ofArray message2Bytes) None None
+    
+    let inputMessage =
+        Compression.compressMessages [message; message2] Protocol.CompressionCodec.Snappy
+
+    let outputMessageSet =
+        Compression.decompressMessage inputMessage Protocol.CompressionCodec.Snappy
 
     let messages = outputMessageSet.messages
     Assert.IsTrue (messages.Length = 2)
