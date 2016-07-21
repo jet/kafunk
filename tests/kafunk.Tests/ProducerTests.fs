cb14e731af61e4d123c2f0ff9bc4ceac6691fc69
Neerav Kothari <neerav@jet.com>
2016-07-21 14:46:20 -0400

    add simple producer tests

--- tests/kafunk.Tests/ProducerTests.fs
+++ tests/kafunk.Tests/ProducerTests.fs
@@ -4,37 +4,41 @@ open NUnit.Framework
 open Kafunk
 open Kafunk.Protocol
 
-let host = "localhost"
+let host = "10.51.23.113"
 
 let port = 9092
 
 let Log = Log.create __SOURCE_FILE__
 
-//[<Test>]
-//let ``simple connection test`` () = 
-//    let conn = Kafka.connHostAndPort host port
-//    let metadata = Kafka.metadata conn (Metadata.Request([||])) |> Async.RunSynchronously
-//    metadata.brokers |> Assert.IsNotEmpty
-
 [<Test>]
-let ``error when writing to unknown topic test`` () = 
-    let topicName = "TopicA"
+let ``simple connection test`` () = 
     let conn = Kafka.connHostAndPort host port
-    let req = ProduceRequest.ofMessageSet topicName 0 (MessageSet.ofMessage (Message.ofBytes "hello world"B (Some "key"B))) None None
-    let res = Kafka.produce conn req |> Async.RunSynchronously
-    if res.topics |> Array.isEmpty then
-        Assert.Fail("No response returned")
-    else
-        let topic,data = 
-            res.topics.[0]
-        if topic = topicName then
-            let x = data |> Array.tryFind(fun (partition, errorCode, offset) -> 
-                printfn "p:%A" partition
-                printfn "ec:%A" errorCode
-                printfn "o:%A" offset
-                errorCode = 0s)
-            Assert.Pass()
-            //|> Array.tryFind(fun (topic,(partition,errorCode,offset)) -> topic = "~~")
-        else
-            printfn ""
-            Assert.Fail("")
+    let metadata = Kafka.metadata conn (Metadata.Request([||])) |> Async.RunSynchronously
+
+    metadata.brokers
+    |> Array.iter(fun b -> printfn "%A" b.nodeId)
+
+    metadata.brokers |> Assert.IsNotEmpty
+
+//[<Test>]
+//let ``error when writing to unknown topic test`` () = 
+//    let topicName = "TopicA"
+//    let conn = Kafka.connHostAndPort host port
+//    let req = ProduceRequest.ofMessageSet topicName 0 (MessageSet.ofMessage (Message.ofBytes "hello world"B (Some "key"B))) None None
+//    let res = Kafka.produce conn req |> Async.RunSynchronously
+//    if res.topics |> Array.isEmpty then
+//        Assert.Fail("No response returned")
+//    else
+//        let topic,data = 
+//            res.topics.[0]
+//        if topic = topicName then
+//            let x = data |> Array.tryFind(fun (partition, errorCode, offset) -> 
+//                printfn "p:%A" partition
+//                printfn "ec:%A" errorCode
+//                printfn "o:%A" offset
+//                errorCode = 0s)
+//            Assert.Pass()
+//            //|> Array.tryFind(fun (topic,(partition,errorCode,offset)) -> topic = "~~")
+//        else
+//            printfn ""
+//            Assert.Fail("")
