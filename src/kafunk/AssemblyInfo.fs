d54e9ec5cabaf6e4b5d4c2dadff7fb2a2d8b18a0
Lev Gorodinski <eulerfx@gmail.com>
2017-01-05 09:23:06 -0500

    update assemblyinfo

--- src/kafunk/AssemblyInfo.fs
+++ src/kafunk/AssemblyInfo.fs
@@ -4,11 +4,11 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("kafunk")>]
 [<assembly: AssemblyProductAttribute("kafunk")>]
 [<assembly: AssemblyDescriptionAttribute("F# client for Kafka")>]
-[<assembly: AssemblyVersionAttribute("0.0.10")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.10")>]
-[<assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Kafunk.Tests")>]
+[<assembly: AssemblyVersionAttribute("0.0.11")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.11")>]
+[<assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("Kafunk.Tests")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.10"
-    let [<Literal>] InformationalVersion = "0.0.10"
+    let [<Literal>] Version = "0.0.11"
+    let [<Literal>] InformationalVersion = "0.0.11"
