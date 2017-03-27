2cbfe0ccde184cf6b15557ff69e9a9906493d9c1
Lev Gorodinski <eulerfx@gmail.com>
2017-03-27 09:42:55 -0400

    Bump version to 0.0.37-alpha001

--- src/kafunk/AssemblyInfo.fs
+++ src/kafunk/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("kafunk")>]
 [<assembly: AssemblyProductAttribute("kafunk")>]
 [<assembly: AssemblyDescriptionAttribute("F# client for Kafka")>]
-[<assembly: AssemblyVersionAttribute("0.0.36")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.36")>]
+[<assembly: AssemblyVersionAttribute("0.0.37")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.37")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.36"
-    let [<Literal>] InformationalVersion = "0.0.36"
+    let [<Literal>] Version = "0.0.37"
+    let [<Literal>] InformationalVersion = "0.0.37"
