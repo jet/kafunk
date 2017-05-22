38c735aad51776c74de8dd285e2c8886d3c1d171
Parvez Fakruddin <parvez@jet.com>
2017-05-22 11:01:47 -0400

    Added snappy compression support

--- src/kafunk/Compression.fs
+++ src/kafunk/Compression.fs
@@ -52,10 +52,56 @@ module GZip =
     let bz = BinaryZipper(output |> Binary.ofArray)
     MessageSet.Read (messageVer, 0, 0s, output.Length, bz)
 
+[<Compile(Module)>]
+module Snappy =
+
+  open System.IO
+  open System.IO.Compression
+  open Snappy
+
+  // The only thing that can be compressed is a MessageSet, not a single Message; this results in a message containing the compressed set
+  let compress (messageVer:ApiVersion) (ms:MessageSet) =
+    // TODO: pool MemoryStreams
+    use outputStream = new MemoryStream()
+    use gZipStream = new SnappyStream(outputStream, CompressionMode.Compress)
+    do
+      let inputBytes = MessageSet.Size (messageVer,ms) |> Array.zeroCreate
+      let buf = Binary.ofArray inputBytes
+      MessageSet.Write (messageVer,ms,BinaryZipper(buf))
+      //MessageSet.write messageVer ms buf |> ignore
+      try
+        gZipStream.Write(inputBytes, 0, inputBytes.Length)
+        gZipStream.Flush()
+      with :? IOException as _ex ->
+        // TODO: log this
+        //printfn "Couldn't write to gzip stream: %A" ex
+        reraise()
+    createMessage (outputStream.ToArray() |> Binary.ofArray) CompressionCodec.Snappy
+
+  let decompress (messageVer:ApiVersion) (m:Message) =
+    let inputBytes = m.value |> Binary.toArray
+    // TODO: pool MemoryStreams
+    use outputStream = new MemoryStream()
+    do
+      use inputStream = new MemoryStream(inputBytes)
+      use gzipInputStream = new SnappyStream(inputStream, CompressionMode.Decompress)
+      try
+        gzipInputStream.CopyTo(outputStream)
+      with :? IOException as _ex ->
+        // TODO: log this
+        //printfn "Couldn't read from gzip stream: %A" ex
+        reraise()
+    outputStream.Position <- 0L
+    let output = outputStream.ToArray()
+    // size is output array size divided by message set element size
+    let bz = BinaryZipper(output |> Binary.ofArray)
+    MessageSet.Read (messageVer, 0, 0s, output.Length, bz)
+
 let compress (messageVer:int16) (compression:byte) (ms:MessageSet) =
   match compression with
   | CompressionCodec.None -> ms
   | CompressionCodec.GZIP -> MessageSet.ofMessage messageVer (GZip.compress messageVer ms)
+  | CompressionCodec.Snappy -> MessageSet.ofMessage messageVer (Snappy.compress messageVer ms)
   | _ -> failwithf "Incorrect compression codec %A" compression
 
 //let decompress (messageVer:int16) (ms:MessageSet) =
@@ -84,6 +130,9 @@ let decompress (messageVer:int16) (ms:MessageSet) =
       | CompressionCodec.None -> [|msi|]
       | CompressionCodec.GZIP ->
         let ms' = GZip.decompress messageVer msi.message
+        ms'.messages
+      | CompressionCodec.Snappy ->
+        let ms' = Snappy.decompress messageVer msi.message
         ms'.messages
       | c -> failwithf "compression_code=%i not supported" c)
     |> MessageSet
--- tests/kafunk.Tests/CompressionGzipTests.fs
+++ tests/kafunk.Tests/CompressionGzipTests.fs
@@ -28,3 +28,25 @@ let ``Compression.GZip should work`` () =
     Assert.IsTrue (msg.value |> Binary.toArray = messageBytes)
     Assert.IsTrue (msg2.value |> Binary.toArray = message2Bytes)
 
+[<Test>]
+[<Category("Compression")>]
+let ``Compression.Snappy should work`` () =
+
+    let messageBytes = [| 1uy; 2uy; 3uy; 4uy; 2uy; 6uy; 8uy |]
+    let message2Bytes = [| 1uy; 2uy; 3uy; 2uy |]
+
+    let message = Message.create (Binary.ofArray messageBytes) (Binary.empty) None
+    let message2 = Message.create (Binary.ofArray message2Bytes) (Binary.empty) None
+    
+    let inputMessage =
+        Compression.Snappy.compress 0s (MessageSet.ofMessages 0s [message; message2])
+
+    let outputMessageSet =
+        Compression.Snappy.decompress 0s inputMessage
+
+    let messages = outputMessageSet.messages
+    Assert.IsTrue (messages.Length = 2)
+    let (offset, size, msg) = let x = messages.[0] in x.offset, x.messageSize, x.message
+    let (offset2, size2, msg2) = let x = messages.[1] in x.offset, x.messageSize, x.message
+    Assert.IsTrue (msg.value |> Binary.toArray = messageBytes)
+    Assert.IsTrue (msg2.value |> Binary.toArray = message2Bytes)
