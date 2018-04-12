2d63aee855c49c2b2a374abc651a652a08717f2e
Vadym Chekan <vadym.chekan@jet.com>
2018-04-12 13:14:44 -0400

    Release notes

--- src/kafunk/AssemblyInfo.fs
+++ src/kafunk/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("kafunk")>]
 [<assembly: AssemblyProductAttribute("kafunk")>]
 [<assembly: AssemblyDescriptionAttribute("F# client for Kafka")>]
-[<assembly: AssemblyVersionAttribute("0.1.16")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.16")>]
+[<assembly: AssemblyVersionAttribute("0.1.17")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.17")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "kafunk"
     let [<Literal>] AssemblyProduct = "kafunk"
     let [<Literal>] AssemblyDescription = "F# client for Kafka"
-    let [<Literal>] AssemblyVersion = "0.1.16"
-    let [<Literal>] AssemblyFileVersion = "0.1.16"
+    let [<Literal>] AssemblyVersion = "0.1.17"
+    let [<Literal>] AssemblyFileVersion = "0.1.17"
